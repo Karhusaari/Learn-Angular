91b1c583cabe29b2c3a8a744da1fab96b9d0e3b5
Doug Bjornholm <doug@bjornholm.net>
2013-12-05 12:27:35 -0500

    testing form elemnts and behaviors

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,40 @@
 var shopModule = angular.module("shop", []);
 
-shopModule.controller("CartController",function($scope){
-	$scope.items = [
-		{title: 'Whiskey', quantity: 1, price: 13.95},
-		{title: 'Vodka', quantity: 4, price: 23.95},
-		{title: 'Beer', quantity: 6, price: 9.99}
-	];
-	
+shopModule.controller("CartController", function($scope) {
+	$scope.items = [{
+		title: 'Whiskey',
+		quantity: 1,
+		price: 13.95
+	}, {
+		title: 'Vodka',
+		quantity: 4,
+		price: 23.95
+	}, {
+		title: 'Beer',
+		quantity: 6,
+		price: 9.99
+	}];
+
+	$scope.header = " : " + $scope.items.length + " items total.";
+
 	$scope.remove = function(index) {
 		$scope.items.splice(index, 1);
+		$scope.header = " : " + $scope.items.length + " items total.";
 	}
+
+	$scope.removeAll = function(){
+		$scope.items = [];
+		$scope.header = ": Your Cart is Empty";
+	}
+
+	$scope.someText = "This is some text for: ";
+
+	$scope.isAwesome = function(){
+		if( $scope.awesome == true){
+			$scope.moreText = " is awesome.";
+		}else{
+			$scope.moreText = "";
+		}
+	};
+	
 });
