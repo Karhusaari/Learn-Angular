293e4e2890b05cb54917492926b85aa835a09f38
Doug Bjornholm <doug@bjornholm.net>
2013-12-12 14:09:20 -0500

    integrated BS and added qty totals

--- js/shop.js
+++ js/shop.js
@@ -15,25 +15,64 @@ shopModule.controller("CartController", function($scope) {
 		price: 9.99
 	}];
 
-	$scope.header = " : " + $scope.items.length + " items total.";
+	$scope.getQuantity = function(){
+		var total = 0;
+		$.each( $scope.items, function(i){
+			var qty = $scope.items[i].quantity;
+			total = Number(total) + Number(qty);
+		});
+		if(total == 0){
+			total = "";
+			return total;
+		}else{
+			return total;
+		}
+		
+	};
+
+	$scope.getTotPrice = function(){
+		var totPrice = 0;
+		$.each( $scope.items, function(i){
+			var qty = $scope.items[i].quantity;
+			totPrice = Number(totPrice) + Number(qty);
+		});
+		if(totPrice == 0){
+			totPrice = "";
+			return totPrice;
+		}else{
+			return totPrice;
+		}
+		
+	};
 
 	$scope.remove = function(index) {
 		$scope.items.splice(index, 1);
-		$scope.header = " : " + $scope.items.length + " items total.";
 	}
+
+	$scope.header = " items";
 
 	$scope.removeAll = function(){
 		$scope.items = [];
-		$scope.header = ": Your Cart is Empty";
+		$scope.header = " Your Cart is Empty";
 	}
-
+	$scope.addText = "";
 	$scope.someText = "This is some text for: ";
 
+	$scope.showAwesome = function(){
+		if( $scope.addText.length > 0){
+			$("#aweCheck").show();
+		}else{
+			$("#aweCheck").hide();
+		}
+	};
+
 	$scope.isAwesome = function(){
-		if( $scope.awesome == true){
+
+		if( $scope.awesome == true && $scope.addText.length > 0){
 			$scope.moreText = " is awesome.";
 		}else{
 			$scope.moreText = "";
+			$scope.awesome = false;
 		}
 	};
 	
